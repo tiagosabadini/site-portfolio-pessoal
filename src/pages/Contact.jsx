34debb02ec5e3db6ca41b69328cb18737aa44ac9
Tiago Sabadini <sabadini.tiago@gmail.com>
2022-06-02 08:20:48 -0300

    Add new pages: Contact and Portfolio

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
 
+//Componentes
+import {ShareFill, PhoneFill,
+  EnvelopeCheckFill, 
+  MapFill, Instagram, Linkedin, Medium} from 'react-bootstrap-icons';
+
 const Contact = () => {
 
   const [visible, setVisible] = useState('');
@@ -12,45 +17,50 @@ const Contact = () => {
       <div className="container">
         <div className="section-title">
           <h2>Entre em contato</h2>
-          <p>Contact Me</p>
         </div>
 
         <div className="row mt-2">
 
         <div className="col-md-6 d-flex align-items-stretch">
           <div className="info-box">
-            <i className="bx bx-map"></i>
-            <h3>My Address</h3>
-            <p>A108 Adam Street, New York, NY 535022</p>
+            <MapFill size="30" color="#a9ada9" />
+            <div>
+              <h3>My Address</h3>
+              <p style={{color: '#5578ff'}}>A108 Adam Street, New York, NY 535022</p>
+            </div>
           </div>
         </div>
 
         <div className="col-md-6 mt-4 mt-md-0 d-flex align-items-stretch">
           <div className="info-box">
-            <i className="bx bx-share-alt"></i>
-            <h3>Social Profiles</h3>
-            <div className="social-links">
-              <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
-              <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
-              <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
-              <a href="#" className="google-plus"><i className="bi bi-skype"></i></a>
-              <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
+            <ShareFill size="30" color="#a9ada9" />
+            <div>
+              <h3>Social Profiles</h3>
+              <div className="social-links">
+                <a href="#" className="instagram"><Instagram color="#5578ff" /></a>
+                <a href="#" className="twitter"><Medium color="#5578ff" /></a>
+                <a href="#" className="linkedin"><Linkedin color="#5578ff" /></a>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="col-md-6 mt-4 d-flex align-items-stretch">
           <div className="info-box">
-            <i className="bx bx-envelope"></i>
-            <h3>Email Me</h3>
-            <p>contact@example.com</p>
+            <EnvelopeCheckFill size="30" color="#a9ada9" />
+            <div>
+              <h3>Email Me</h3>
+              <p style={{color: '#5578ff'}}>contact@example.com</p>
+            </div>
           </div>
         </div>
         <div className="col-md-6 mt-4 d-flex align-items-stretch">
           <div className="info-box">
-            <i className="bx bx-phone-call"></i>
-            <h3>Call Me</h3>
-            <p>+1 5589 55488 55</p>
+            <PhoneFill size="30" color="#a9ada9" />
+            <div>
+              <h3>Call Me</h3>
+              <p style={{color: '#5578ff'}}>+1 5589 55488 55</p>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 
+//Componentes
+import {WindowStack} from 'react-bootstrap-icons';
+
 const Portfolio = () => {
 
   const [visible, setVisible] = useState('');
@@ -13,82 +16,37 @@ const Portfolio = () => {
         <div className="section-title">
           <h2>Portfolio</h2>
         </div>
-
+        
         <div className="row">
           <div className="col-lg-3 col-md-4">
             <div className="icon-box">
-              <i className="ri-store-line" style={{color: '#ffbb2c'}}></i>
-              <h3>Lorem Ipsum</h3>
+              <WindowStack color="#5578ff" />
+              <h3>Projeto Um</h3>
+              <a href="https://projetoum.com.br" target="_blank" className="" rel="noreferrer">https://projetoum.com.br</a>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4 mt-md-0">
             <div className="icon-box">
-              <i className="ri-bar-chart-box-line" style={{color: '#5578ff'}}></i>
-              <h3>Dolor Sitema</h3>
+            <WindowStack color="#5578ff" />
+              <h3>Projeto Dois</h3>
+              <a href="https://projetodois.com.br" target="_blank" className="" rel="noreferrer">https://projetodois.com.br</a>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4 mt-md-0">
             <div className="icon-box">
-              <i className="ri-calendar-todo-line" style={{color: '#e80368'}}></i>
-              <h3>Sed perspiciatis</h3>
+            <WindowStack color="#5578ff" />
+              <h3>Projeto Três</h3>
+              <a href="https://projetotres.com.br" target="_blank" className="" rel="noreferrer">https://projetotres.com.br</a>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4 mt-lg-0">
             <div className="icon-box">
-              <i className="ri-paint-brush-line" style={{color: '#e361ff'}}></i>
-              <h3>Magni Dolores</h3>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-4 mt-4">
-            <div className="icon-box">
-              <i className="ri-database-2-line" style={{color: '#47aeff'}}></i>
-              <h3>Nemo Enim</h3>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-4 mt-4">
-            <div className="icon-box">
-              <i className="ri-gradienter-line" style={{color: '#ffa76e'}}></i>
-              <h3>Eiusmod Tempor</h3>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-4 mt-4">
-            <div className="icon-box">
-              <i className="ri-file-list-3-line" style={{color: '#11dbcf'}}></i>
-              <h3>Midela Teren</h3>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-4 mt-4">
-            <div className="icon-box">
-              <i className="ri-price-tag-2-line" style={{color: '#4233ff'}}></i>
-              <h3>Pira Neve</h3>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-4 mt-4">
-            <div className="icon-box">
-              <i className="ri-anchor-line" style={{color: '#b2904f'}}></i>
-              <h3>Dirada Pack</h3>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-4 mt-4">
-            <div className="icon-box">
-              <i className="ri-disc-line" style={{color: '#b20969'}}></i>
-              <h3>Moton Ideal</h3>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-4 mt-4">
-            <div className="icon-box">
-              <i className="ri-base-station-line" style={{color: '#ff5828'}}></i>
-              <h3>Verdo Park</h3>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-4 mt-4">
-            <div className="icon-box">
-              <i className="ri-fingerprint-line" style={{color: '#29cc61'}}></i>
-              <h3>Flavor Nivelanda</h3>
+            <WindowStack color="#5578ff" />
+              <h3>Projeto Quatro</h3>
+              <a href="https://projetoquatro.com.br" target="_blank" className="" rel="noreferrer">https://projetoquatro.com.br</a>
             </div>
           </div>
         </div>
-
       </div>
     </section>
   );
